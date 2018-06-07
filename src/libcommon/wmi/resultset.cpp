559f23a70c358285d50b56f53fb767894046e21d
Odd Stranne <odd@mullvad.net>
2018-06-07 13:25:05 +0200

    Add classes for working with WMI

---
+++ src/libcommon/wmi/resultset.cpp
@@ -0,0 +1,33 @@
+#include "stdafx.h"
+#include "resultset.h"
+#include "libcommon/com.h"
+
+namespace common::wmi
+{
+
+ResultSet::ResultSet(CComPtr<IEnumWbemClassObject> rs) : m_resultset(rs)
+{
+}
+
+bool ResultSet::advance()
+{
+	if (nullptr != m_result)
+	{
+		m_result.Release();
+	}
+
+	ULONG dummy;
+
+	const auto status = m_resultset->Next(WBEM_INFINITE, 1, &m_result, &dummy);
+
+	VALIDATE_COM(status, "Retrieve next object in COM resultset");
+
+	return WBEM_S_FALSE != status;
+}
+
+CComPtr<IWbemClassObject> ResultSet::result()
+{
+	return m_result;
+}
+
+}
