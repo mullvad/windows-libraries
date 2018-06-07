cfd30a10f7de339306b38653cbb8820b60dad655
Odd Stranne <odd@mullvad.net>
2018-06-07 22:28:49 +0200

    Correct renaming failure in migrated code

--- src/libcommon/wmi/wmi.cpp
+++ src/libcommon/wmi/wmi.cpp
@@ -5,24 +5,24 @@
 namespace common::wmi
 {
 
-_variant_t ComGetProperty(CComPtr<IWbemClassObject> obj, const std::wstring &name)
+_variant_t WmiGetProperty(CComPtr<IWbemClassObject> obj, const std::wstring &name)
 {
 	_variant_t val;
 
 	const auto status = obj->Get(name.c_str(), 0, &val, nullptr, nullptr);
 
-	VALIDATE_COM(status, "Retrieve COM property value");
+	VALIDATE_COM(status, "Retrieve WMI property value");
 
 	return val;
 }
 
-_variant_t ComGetPropertyAlways(CComPtr<IWbemClassObject> obj, const std::wstring &name)
+_variant_t WmiGetPropertyAlways(CComPtr<IWbemClassObject> obj, const std::wstring &name)
 {
-	auto val = ComGetProperty(obj, name);
+	auto val = WmiGetProperty(obj, name);
 
 	if (VT_EMPTY == V_VT(&val) || VT_NULL == V_VT(&val))
 	{
-		throw std::runtime_error("A required COM property value is empty.");
+		throw std::runtime_error("A required WMI property value is empty.");
 	}
 
 	return val;
